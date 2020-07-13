34d91a3fe22ca85133b62740c826b70fa11815e5
dgwedo <duongvu98832@gmail.com>
2020-07-13 18:22:10 +0700

    update

--- testWebFull.py
+++ testWebFull.py
@@ -2,21 +2,20 @@ for i in range(4):
     for j in range(4):
         print("_  ",end='')
     print()
+def printBroadCurrent(column,row):
+    for target1 in range(4):
+            for target2 in range(4):
+                if target2 == column and target1 == row-1:
+                    print('x  ',end='')
+                else:
+                    print('_  ',end='')
+            print()
 
 while True:
     inp = input("yes/no:(y/n)")
     if inp == 'y':
         row = int(input(">> row: "))
         column = int(input(">> column: "))
-        for target1 in range(4):
-            for target2 in range(4):
-                if target2 == row:
-                    print('x ',end='')
-                else:
-                    print("_  ",end='')
-            if target1 == column:
-                print('x')
-            else:
-                print()
+        printBroadCurrent(column,row)
     if inp =='n':
         break
